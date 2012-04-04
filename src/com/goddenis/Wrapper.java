8cf4da38bafa303322dcc4db2cb5af148257a3c6
goddenis <goddenis@yandex.ru>
2012-04-04 22:07:58 +0700

    libs added

--- src/com/goddenis/Wrapper.java
+++ src/com/goddenis/Wrapper.java
@@ -1,16 +1,24 @@
 package com.goddenis;
 
+import org.xBaseJ.DBF;
+import sun.jdbc.odbc.JdbcOdbcConnection;
+
+import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.List;
 
 /**
  * User: goddenis
  * Date: 04.04.12
  */
 public class Wrapper {
-    public static void main(String[] args) {
-        List<String> strings = new ArrayList<String>();
-
-
+    public static void main(String args[]) {
+        ArrayList<String> strings = new ArrayList<String>();
+        DBF
+        try {
+            sun.jdbc.odbc.JdbcOdbcDriver
+        } catch (SQLException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+        JdbcOdbcConnection connection = new JdbcOdbcConnection();
     }
 }
