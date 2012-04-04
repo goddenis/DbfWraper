2afd24ace20a659226c9ae1d05599de3142c5f46
goddenis <goddenis@yandex.ru>
2012-04-04 21:11:53 +0700

    main add

--- src/com/goddenis/Wrapper.java
+++ src/com/goddenis/Wrapper.java
@@ -1,11 +1,16 @@
 package com.goddenis;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * User: goddenis
  * Date: 04.04.12
  */
 public class Wrapper {
     public static void main(String[] args) {
+        List<String> strings = new ArrayList<String>();
+
 
     }
 }
