8dd73bde4701158531d13ded335199a3e7ce176a
goddenis <goddenis@yandex.ru>
2012-04-04 21:02:42 +0700

    main add

--- src/com/goddenis/Wrapper.java
+++ src/com/goddenis/Wrapper.java
@@ -1,11 +1,11 @@
 package com.goddenis;
 
 /**
- * Created with IntelliJ IDEA.
  * User: goddenis
  * Date: 04.04.12
- * Time: 0:08
- * To change this template use File | Settings | File Templates.
  */
 public class Wrapper {
+    public static void main(String[] args) {
+
+    }
 }
